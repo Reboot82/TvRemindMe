d234cdef15c93387c1cd9e5ebb3a95ad78247701
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2019-11-10 15:08:59 -0500

    save point 6

--- client/src/components/SingleService.js
+++ client/src/components/SingleService.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react'
 import axios from 'axios'
-import Show from './Shows'
 import { Link } from 'react-router-dom';
 
 /* Step 2
@@ -27,51 +26,44 @@ export default class SingleService extends Component {
             })
     }
 
-    // componentDidMount() {
-    // const serviceId = this.props.match.params.serviceId
-    // axios.get(`/api/services/${serviceId}`)
-    //     .then((res) => {
-    //         console.log(res.data)
-    //         const previousState = {...this.state}
-    //         const newState = Object.assign({}, previousState, res.data)
-    //          this.setState(newState)
-    //     })
-
-    //  axios.get(`api/shows/byServiceId/${serviceId})
-    //      .then((res) => {
-    //         console.log(res.data)
-    //         const previousState = {...this.state}
-    //         const newState = Object.assign({shows: []}, previousState, { shows: res.data })
-    //          this.setState(newState)
-    //     })
-    // }
-
     componentDidMount() {
         const serviceId = this.props.match.params.serviceId
         axios.get(`/api/services/${serviceId}`)
             .then((res) => {
-                this.setState(res.data)
+                console.log(res.data)
+                const previousState = { ...this.state }
+                const newState = Object.assign({}, previousState, res.data)
+                this.setState(newState)
+            })
+
+        axios.get(`api/shows/byServiceId/${serviceId}`)
+            .then((res) => {
+                console.log(res.data)
+                const previousState = { ...this.state }
+                const newState = Object.assign({ shows: [] }, previousState, { shows: res.data })
+                this.setState(newState)
             })
     }
 
     render() {
         return (
+            <div>
                 <div>
-                    <div>
-                        <h1>{this.state.name}</h1>
-                        <h3>Shows</h3>
-                        {this.state.showList.map((show) => {
-                            return (<p>{show.name}</p>)
-                        })}
-                        <Show showList={this.props.showList} />
-                    </div>
-                   
-                    <span>
-                        <Link to={"/"}>
-                            <button>Home</button>
-                        </Link>
-                    </span>
+                    <h1>{this.state.name}</h1>
+                    <h3>Shows</h3>
+                    <Link to={`/${this.state.showList.show._id}`} >
+                    {this.state.showList.map((show) => {
+                        return (<p>{show.name}</p>)
+                    })}
+                    </Link>
                 </div>
+
+                <span>
+                    <Link to={"/"}>
+                        <button>Home</button>
+                    </Link>
+                </span>
+            </div>
 
         )
     }
